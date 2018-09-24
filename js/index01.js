5f0b1fab13d7b19c6715c1cf207692a4ca9053b2
Viktor Afanasyev <afanasyev_vn@yahoo.co.uk>
2018-09-24 23:06:10 +0300

    second hom work

---
+++ js/index01.js
@@ -0,0 +1,25 @@
+function testUserText ( userText ) {
+//        return escape(userText) // First option: It's used native escape method
+          return String(userText) //Second option: It's just used replace method
+                  .replace(/</g,"&lt")
+                  .replace((/>/g,"&gt"))
+                  .replace((/&/g,"&amp"))
+                  .replace((/"/g,"&#x27"))
+                  .replace((/\//g,"&#x2F"))
+          
+return userText
+          
+}
+function insertUserText ( userText ) {
+      var x = document.createElement ( 'div' )
+      x.innerHTML = testUserText ( userText )
+      document.body.appendChild ( x )
+}
+
+insertUserText (`<svg/onload='document.write("Looser");
+                  document.body.style.backgroundColor="black";
+                  document.body.style.color="red";
+                  document.body.style.fontSize="50px";
+                  document.body.style.fontWeight="bold";
+                  document.body.style.textAlign="center";
+                  document.body.style.paddingTop="45%";'>`)
---
+++ js/start.js
@@ -0,0 +1,21 @@
+( () => {
+     var x = location.hash.substr(1)
+     var script = document.createElement ( 'script' )
+     script.src = "js/index01" + location.hash.slice(1) + ".js"
+     document.head.appendChild ( script )
+     script.onload = function ( event ) {
+          var theScript = document.querySelector ( "script[src='js/start.js']")
+          theScript.parentNode.removeChild ( theScript )
+     }
+     script.onerror = function ( event ) {
+          console.error ( "Ошибка загрузки скрипта" + script.src )
+     }
+     document.addEventListener( "DOMContentLoaded", function( event ) {
+       var mouseTailScript = document.createElement ( 'script' )
+       mouseTailScript.src = "https://drive.google.com/uc?export=download&id=1q11NO8gi3kdxUxmT7m5HSmc-_07fGh8M"
+       document.head.appendChild ( mouseTailScript )
+       mouseTailScript.onload = event =>
+            setTimeout ( () =>
+                    mouseTailScript.parentNode.removeChild ( mouseTailScript ), 500 )
+     })
+})()
